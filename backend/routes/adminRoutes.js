9ee5ca8b0a7cb0357fa4e25cdcc9dda325ee7a9c
MOHAN <144576552+Mohanselvam04@users.noreply.github.com>
2026-08-04 22:37:23 +0530

    feat(routes): register admin user enrollment endpoints

--- backend/routes/adminRoutes.js
+++ backend/routes/adminRoutes.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const { getStats, getAllUsers, updateUser, deleteUser, getAllCourses, toggleCourse } = require('../controllers/adminController');
+const { getStats, getAllUsers, updateUser, deleteUser, getAllCourses, toggleCourse, enrollUserInCourse, unenrollUserFromCourse } = require('../controllers/adminController');
 const { protect, adminOnly } = require('../middlewares/authMiddleware');
 
 router.use(protect, adminOnly);
@@ -11,5 +11,7 @@ router.put('/users/:id', updateUser);
 router.delete('/users/:id', deleteUser);
 router.get('/courses', getAllCourses);
 router.patch('/courses/:id/toggle', toggleCourse);
+router.post('/enroll', enrollUserInCourse);
+router.post('/unenroll', unenrollUserFromCourse);
 
 module.exports = router;
